361e9fd95564a247abee0835bbb3f65e0b19b38e
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-20 13:57:32 +0530

    Initial Commit

---
+++ Hot Prospects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hot Prospects
+//
+//  Created by Mukthar Amiyan on 20/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Mukthar Amiyan on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
