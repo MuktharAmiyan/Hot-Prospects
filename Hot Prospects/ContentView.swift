e478fa206e52f2f8ecad05c60df665aac8f2f4bd
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-21 12:07:28 +0530

    Result type

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -7,36 +7,40 @@
 
 import SwiftUI
 
-
-@MainActor class DelayedUpdater: ObservableObject {
-    var value = 0 {
-        willSet{
-            objectWillChange.send()
-        }
-    }
-    
-    
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i) ){
-                
-                self.value += 1
-            }
-        }
-    }
-}
-
 struct ContentView: View {
-    @StateObject private var updater = DelayedUpdater()
-    
+    @State private var output = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Value is \(updater.value)")
+            Text(output)
         }
         .padding()
+        .task {
+            await fetchReadings()
+        }
+    }
+    
+    func fetchReadings()async {
+        let fetchTask = Task{ () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data,_) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) Readings"
+        }
+        
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Download Error \(error.localizedDescription)"
+         
+        }
+        
     }
 }
 
