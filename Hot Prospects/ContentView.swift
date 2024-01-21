ef13f8b275b384ce367e83c08b051d5a81eb4c62
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-21 12:30:45 +0530

    Context menu

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -8,39 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
+    @State private var backgroundColor = Color.red
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(output)
+        VStack{
+          Text("Hello World!")
+                .padding()
+                .background(backgroundColor)
+          Text("Context Menu")
+                .padding()
+                .contextMenu {
+                    Button (role: .destructive){
+                        backgroundColor = .red
+                    }label: {
+                        Label("Red", systemImage: "checkmark.circle.fill")
+                    }
+                    Button (role: .destructive){
+                        backgroundColor = .blue
+                    }label: {
+                        Label("Blue", systemImage: "checkmark.circle.fill")
+                    }
+                    Button (role: .destructive){
+                        backgroundColor = .green
+                    }label: {
+                        Label("Green", systemImage: "checkmark.circle.fill")
+                    }
+                }
         }
-        .padding()
-        .task {
-            await fetchReadings()
-        }
-    }
-    
-    func fetchReadings()async {
-        let fetchTask = Task{ () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data,_) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) Readings"
-        }
-        
-        
-        let result = await fetchTask.result
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Download Error \(error.localizedDescription)"
-         
-        }
-        
     }
 }
 
