80ec826fba101d61ffec24a098418e93058a1a67
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-21 14:33:21 +0530

    git hub login

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -6,36 +6,16 @@
 //
 
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
     var body: some View {
-        VStack{
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) 
-                { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the dogs"
-                content.subtitle = "They look hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
