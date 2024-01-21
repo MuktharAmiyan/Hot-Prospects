2cb9e03f8f6fabdad34f8972cffc9ad15d5bc127
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-21 14:46:02 +0530

    add package

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -5,17 +5,27 @@
 //  Created by Mukthar Amiyan on 20/01/24.
 //
 
+import SamplePackage
 import SwiftUI
 
+
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! \n \(results)")
+                .multilineTextAlignment(.center)
         }
         .padding()
+    }
+    
+    var results:String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
     }
 }
 
